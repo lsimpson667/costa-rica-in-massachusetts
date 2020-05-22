e623c34f38cb15d8c8a511a0d2b7fb113a1228cd
Lois Simpson <lsimpson667@gmail.com>
2020-05-22 18:57:58 -0400

    first backend update

---
+++ public/scripts/script.js
@@ -0,0 +1,39 @@
+// Slick slider slide show starts here 
+$(document).ready(() => {
+    $(".photo-albums-subcontainer").slick( {
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 1950,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+
+});
+})
