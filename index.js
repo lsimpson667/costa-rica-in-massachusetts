068327921e5538998aa17b59f40863c42afab416
Lois Simpson <lsimpson667@gmail.com>
2020-06-05 18:36:02 -0400

    updated footer and added hover states

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 const express = require('express');  
-const app = express();	          
 const bodyParser = require('body-parser');
-const path = require('path');
 const mongodb = require('mongodb');
 const dotenv = require('dotenv');
-dotenv.config();
+const passport = require('passport'); // adding passport
+const LocalStrategy = require('passport-local').Strategy; // adds a strategy called Passport Local
+const flash = require('connect-flash'); // gives error messages to Passport
+
+const path = require('path');
 
 const PORT = process.env.PORT || 9000;
+const app = express();
+dotenv.config();
 
 app.set("view engine", "ejs");   
 
 app.use(express.static(path.join(__dirname, 'public'))); 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( {extended: true} ));
+app.use(require('express-session')({ 
+    //REQUIRED key value pair - This is the secret used to sign the session ID cookie. This can be either a string for a single secret, or an array of multiple secrets. Before production, this should be secret: process.env.SESSION_SECRET. Real code should be in .env file
+    secret: 'keyboard cat', 
+    // optional key/value pair - Forces the session to be saved back to the session store, even if the session was never modified during the request. 
+    resave: false, 
+    // optional key/value pair - Forces a session that is "uninitialized" to be saved to the store. A session is uninitialized when it is new but not modified. Choosing false is useful for implementing login sessions, reducing server storage usage, or complying with laws that require permission before setting a cookie. Choosing false will also help with race conditions where a client makes multiple parallel requests without a session.
+    saveUninitialized: false 
+  }));
+  app.use(flash()); // using connect-flash to show error messages
+  
+// Initialize Passport and restore authentication state, if any, from the session.
+app.use(passport.initialize()); // middleware is required to initialize Passport
+app.use(passport.session()); // needed if the app is persistent login sessions (MongoDB)
 
 // Declare any constants or variables here for Database
 let db_handler;
@@ -21,6 +38,7 @@ const DB_URL = "mongodb://localhost:27017";
 const DB_NAME = "costaricaProjectDB";
 const USER_COLLECTION = "users";
 const EVENT_COLLECTION = "events";
+const ADMIN_COLLECTION = "admin"
 
 app.listen(PORT, () => {
     console.log(`Server Started on Port: ${PORT}`);
@@ -41,12 +59,120 @@ app.listen(PORT, () => {
 
 // GET, GET, POST, GET, GET, PUT, DELETE
 app.get('/', (request, response) => { 
-    
     response.render('index', { 
-        
+        thanks: request.query.thanks
+
     });
 });
 
+app.get('/login',
+(req, res) => {
+  res.render('login');
+});
+
+app.post('/login', 
+// passport.authenticate with the local strategy will handle the login request.
+  passport.authenticate('local', {
+    failureRedirect: '/login', 
+    successRedirect: '/',
+    failureFlash: true // NEW! this will turns on the error message for connect-flash
+  })
+);
+
+app.get('/register',
+(req, res) => {
+  res.render('register');
+});
+
+app.post('/register', (req, res) => {
+  const form_data = req.body;
+  const username = form_data['username'];
+  const password = form_data['password']
+  const displayName = form_data['displayname']
+  const email = form_data['email']
+
+  const my_object = {
+      username: username,
+      password: password,
+      displayname: displayName,
+      email: email
+  }
+
+  db_handler.collection(COLLECTION_NAME).insertOne(my_object, (err, result) => {
+      if (err) {
+          console.log("Error: " + err);
+      }
+      else {
+          console.log("One Entry Added");
+          res.redirect('/login');
+      }
+  });
+});
+  
+app.get('/logout',
+  (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
+
+  // connect-ensure-login is a middleware ensures that a user is logged in. If a request is received that is unauthenticated, the request will be redirected to a login page. The URL will be saved in the session, so the user can be conveniently returned to the page that was originally requested.
+  app.get('/profile',
+  require('connect-ensure-login').ensureLoggedIn(),
+  (req, res) => {
+    db_handler.collection(COLLECTION_NAME).find({username: req.user.username}).toArray((err, user) => {
+      if(err) return console.log(err)
+      let userObject = user[0]
+      console.log(userObject)
+      if(user) res.render('profile',{user: userObject})
+    })
+   
+  });
+
+// Configure the local strategy for use by Passport.
+//
+// The local strategy require a `verify` function which receives the credentials
+// (`username` and `password`) submitted by the user.  The function must verify
+// that the password is correct and then invoke `cb` with a user object, which
+// will be set at `req.user` in route handlers after authentication.
+passport.use(new LocalStrategy(
+    (username, password, done) => {
+     
+      db_handler.collection(COLLECTION_NAME).find({username: username}).toArray((err, user) => {
+        if(err){
+          return err;
+        }
+        if(!user){
+          return done(null, false);
+        }
+        if(user[0].password != password) {
+          return done(null, false);
+        }
+        let userObject = user[0];
+  
+        if(user) {
+          return done(null, userObject)
+        }
+      })
+    }
+  ));
+  
+  
+  
+  // Configure Passport authenticated session persistence.
+  //
+  // In order to restore authentication state across HTTP requests, Passport needs
+  // to serialize users into and deserialize users out of the session.  The
+  // typical implementation of this is as simple as supplying the user ID when
+  // serializing, and querying the user record by ID from the database when
+  // deserializing.
+  passport.serializeUser((user, done) => {
+    done(null, user.username);
+  });
+  
+  passport.deserializeUser((username, done) => {
+    done(null, {username: username});
+  }); 
+  
 // ROUTE FOR USER ENTRIES STARTS HERE >>>>>>>>>>>>>>>>>>>>>>>>>>>>
 //  I don't think this should be a post
 app.post('/add', (req, res) => {
@@ -88,8 +214,8 @@ app.post('/add', (req, res) => {
             // redirect sends client to a specified route.
             
             // redirect me to this path
-            res.redirect('/#submit-container');
-
+            res.redirect('/?thanks=yes#HearAboutUs');
+            
         }
     })
 });
@@ -137,7 +263,27 @@ app.get('/admin/add_event', (request, response) => {
     });
 });
 
-// This link posts new events in several places (below the addd event form, on events.ejs page, and update event page)
+// This route shows a page with nothing but the event name and it's details
+app.get('/event_details/:event_id', (request, response) => { 
+    const parameters = req.params;
+    // converted into an object Id for mongo db compass
+    const event_id = mongodb.ObjectId(parameters['event_id']); 
+
+    db_handler.collection(EVENT_COLLECTION).find({_id: event_id}).toArray( (err, result) => {
+        if (err) {
+            res.send("EVENT NOT FOUND!");
+            console.log(err);
+        }
+        else{
+            // render it to show the page with specified info on it. (IT IS NOT A ROUTE!)
+            response.render('event_details', {
+            event: result[0]
+            });
+        }
+    });
+});
+
+    // This link posts new events in several places (below the addd event form, on events.ejs page, and update event page)
 app.post('/add_event', (request, response) => { 
     const form_data = request.body;
     console.log(request.body);
@@ -146,12 +292,14 @@ app.post('/add_event', (request, response) => {
     const event_date = form_data['eventDate'];
     const event_time = form_data['eventTime'];
     const event_location = form_data['eventLocation'];
+    const event_additionalInfo = form_data['eventAdditionalInfo'];
 
     const event_obj = {
         event_name: event_name,
         event_date: event_date, 
         event_time: event_time,
-        event_location: event_location
+        event_location: event_location,
+        event_additionalInfo: event_additionalInfo
     }
     console.log(event_obj);
 
@@ -212,6 +360,7 @@ app.post('/update_event/:event_id', (req, res)  => {
     const event_date = form_data['eventDate'];
     const event_time = form_data['eventTime'];
     const event_location = form_data['eventLocation'];
+    const event_additionalInfo = form_data['eventAdditionalInfo'];
 
     const new_values = {$set: {event_name: event_name, event_date: event_date, event_time: event_time, event_location: event_location}};
 
@@ -277,3 +426,8 @@ app.get('/events', (req, res) => {
 app.post('', (req, res) => {
 
 });
+
+
+
+
+
